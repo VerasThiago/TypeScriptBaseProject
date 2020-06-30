17942fc554c061e48c4ccd01b47123ccdbcb3b7d
Thiago Veras <thiago.veras.machado@gmail.com>
2020-06-30 16:37:05 -0300

    basic project

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { helloWorld } from './module/basicFunction'
 
 async function print() {
-    console.log(await helloWorld())
+  console.log(await helloWorld())
 }
 
 print()
--- src/module/basicFunction.ts
+++ src/module/basicFunction.ts
@@ -1,8 +1,9 @@
-function delay(ms: number){
-  return new Promise(resolve => setTimeout(resolve, ms));
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
 export const helloWorld = async () => {
-    await delay(2000)
-    return "Hello World"
+  await delay(2000)
+
+  return 'Hello World'
 }
